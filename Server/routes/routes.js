35b7a59fd5c52fb456c4778527cb204876434c8c
Daniil <palidos@yandex.ru>
2020-05-11 20:17:55 +0300

    Add nextTest button and corresponding request

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -13,37 +13,7 @@ const router = Router();
 // asd.save(error => { if (error) { console.log(error); } else { console.log('saved'); } });
 
 router.post('/api/questions', async (req, res) => {
-  if (req.body.length) {
-    const a = [];
-    req.body.map(async options => {
-      console.log(options, 'options');
-      console.log(await InitialLevel.find(options, 'theme').lean().exec(), 'asasasd');
-      InitialLevel.find(options).lean().exec((err, doc) => a.push(doc));
-    });
-    return res.status(200).send(a);
-  // if (req.body.length) {
-  //   req.body.map(async options => {
-  //     console.log(options, 'options');
-  //     console.log(await InitialLevel.find(options, 'theme').lean().exec(), 'asasasd');
-  //     InitialLevel.find(options, (err, data) => res.json(data));
-  //   });
-  //   return res.status(200);
-  }
-  // await InitialLevel.find({ questionLevel: 0 }, (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.sendStatus(500);
-  //   }
-  //   if (!data) {
-  //     return res.status(404).json({ err: 'No data' });
-  //   }
-  //   return res.status(200).send(data);
-  // });
-});
-
-
-router.post('/api/questions/submit', async (req, res) => {
-  await InitialLevelAnswers.find((err, data) => {
+  InitialLevel.find({ $or: req.body }).exec((err, data) => {
     if (err) {
       console.log(err);
       return res.sendStatus(500);
@@ -52,6 +22,31 @@ router.post('/api/questions/submit', async (req, res) => {
       return res.status(404).json({ err: 'No data' });
     }
     return res.status(200).send(data);
+  });
+});
+
+router.post('/api/questions/submit', async (req, res) => {
+  const options = mode => req.body.map(option => ({ [mode ? 'questionId' : '_id']: option.questionId }));
+  await InitialLevelAnswers.find({ $or: options(1) }).lean().exec((err, docs) => {
+    InitialLevel.find({ $or: options(0) }).lean().exec((error, data) => {
+      if (err || error) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      if (!docs) {
+        return res.status(404).json({ err: 'No data' });
+      }
+      return res.status(200).send(docs.map(doc => ({
+        ...doc,
+        correct: doc.answers.some(answer =>
+          answer === req.body.find(({ questionId }) =>
+            questionId === doc.questionId.toString()).answer),
+        theme: data.find(({ _id }) =>
+          _id.toString() === doc.questionId.toString()).theme,
+        questionLevel: data.find(({ _id }) =>
+          _id.toString() === doc.questionId.toString()).questionLevel,
+      })));
+    });
   });
 });
 
